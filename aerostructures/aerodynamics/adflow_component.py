76f27c312dec665737ff72591eafd98a4ef46432
Gil231b <gil.ruiz.jimenz@gmail.com>
2020-02-03 14:20:04 +0100

    indentation adjustments

--- aerostructures/aerodynamics/adflow_component.py
+++ aerostructures/aerodynamics/adflow_component.py
@@ -25,7 +25,7 @@ class ADflow(Component):
         #Symmetry plane index
         self.sym_plane_index = sym_plane_index
 
-		#Surface points coordinates (jig shape)
+        #Surface points coordinates (jig shape)
         self.add_param('jig_surface_coord', val=np.zeros((na,3)))
         
         #Wing reference area (full aircraft configuration)
@@ -67,49 +67,49 @@ class ADflow(Component):
 
 
     def solve_nonlinear(self, params, unknowns, resids):
-		
-		jig_surface_coord = params['jig_surface_coord']
-		
-		Sw = params['Sw']
-		
-		h = params['h']
-		
-		delta = params['delta']
-		
-		Mach = params['Mach']
-		
-		alpha = params['alpha']
-		
-		c = params['c']
-		
-		sym_plane_index = self.sym_plane_index
+        
+        jig_surface_coord = params['jig_surface_coord']
+        
+        Sw = params['Sw']
+        
+        h = params['h']
+        
+        delta = params['delta']
+        
+        Mach = params['Mach']
+        
+        alpha = params['alpha']
+        
+        c = params['c']
+        
+        sym_plane_index = self.sym_plane_index
 
-		#Add surface displacements to jig shape
-		aero_surf_coord = jig_surface_coord + delta
-		
-		#Enforce symmetry condition, if existing
-		if sym_plane_index is not None:
-			j = sym_plane_index - 1
-			for i in range(len(jig_surface_coord)):
-				if jig_surface_coord[i][j] == 0.:
-					aero_surf_coord[i][j] = 0.
-			#Use half-surface as the reference surface if symmetry exists
-			Sw = Sw/2.
+        #Add surface displacements to jig shape
+        aero_surf_coord = jig_surface_coord + delta
+        
+        #Enforce symmetry condition, if existing
+        if sym_plane_index is not None:
+            j = sym_plane_index - 1
+            for i in range(len(jig_surface_coord)):
+                if jig_surface_coord[i][j] == 0.:
+                    aero_surf_coord[i][j] = 0.
+            #Use half-surface as the reference surface if symmetry exists
+            Sw = Sw/2.
 
-		self.CFDSolver.setSurfaceCoordinates(aero_surf_coord)
+        self.CFDSolver.setSurfaceCoordinates(aero_surf_coord)
 
-		# Aerodynamic problem description
-		ap = AeroProblem(name=self.name, alpha=alpha, mach=Mach, altitude=h,
-						 areaRef=Sw, chordRef=c, evalFuncs=['cl','cd'])
+        # Aerodynamic problem description
+        ap = AeroProblem(name=self.name, alpha=alpha, mach=Mach, altitude=h,
+                         areaRef=Sw, chordRef=c, evalFuncs=['cl','cd'])
 
-		# Solve and evaluate functions
-		funcs = {}
-		self.CFDSolver(ap)
-		self.CFDSolver.evalFunctions(ap, funcs)
+        # Solve and evaluate functions
+        funcs = {}
+        self.CFDSolver(ap)
+        self.CFDSolver.evalFunctions(ap, funcs)
 
-		# Set the value of the unkowns
-		unknowns['f_a'] = self.CFDSolver.getForces()
+        # Set the value of the unkowns
+        unknowns['f_a'] = self.CFDSolver.getForces()
 
-		unknowns['CL'] = funcs[self.name + '_cl']
+        unknowns['CL'] = funcs[self.name + '_cl']
 
-		unknowns['CD'] = funcs[self.name + '_cd']
+        unknowns['CD'] = funcs[self.name + '_cd']
